b4986287b99fb437a9f08ac250d0a48245a250ef
sctrandrei <sctr.andrei@gmail.com>
2020-10-26 19:16:58 +0200

    add service

---
+++ Students/Students.Api/Controllers/StudentsController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Students.Data;
+using Students.Models;
+
+namespace Students.Api.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // nu e recomandat
+        //[HttpGet("")]
+        //public string TestGet()
+        //{
+        //    return "primul string";
+        //}
+
+        // e recomandat (nu trebuie sa intoarcem obiect care nu e de tipul HttpResult)
+        //[HttpGet("")]
+        //public IActionResult TestGetV2()
+        //{
+        //     return this.Ok("primul string");
+        //}
+
+            // intoarce toata lista 
+        [HttpGet("")]
+        public IActionResult GetAll()
+        {
+            var manager = new StudentManager();
+
+            var students = manager.GetAll();
+
+            return this.Ok(students);
+        }
+
+            // intoarce element din list dupa id
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var manager = new StudentManager();
+
+            var student = manager.GetById(id);
+
+            if (student == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(student);
+        }
+
+            // creeaza un nou element in lista
+        [HttpPost("")]
+        public IActionResult CreateStudent(Student student)
+        {
+            var manager = new StudentManager();
+
+            manager.Add(student);
+
+            return this.Created($"api/students/{student.Id.ToString()}", student);
+        }
+
+            // delete element dupa id
+        [HttpDelete("{id}")]
+        public IActionResult CreateStudent(int id)
+        {
+            var manager = new StudentManager();
+
+            manager.Delete(id);
+
+            return this.Ok();
+        }
+
+            // face update la element dupa id cu noul model
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id, Student model)
+        {
+            var manager = new StudentManager();
+
+            manager.Update(id, model);
+
+            return this.Ok();
+        }
+    }
+}
---
+++ Students/Students.Data/StudentManager.cs
@@ -0,0 +1,61 @@
+﻿using Students.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Students.Data
+{
+    public class StudentManager
+    {
+        static List<Student> studentList = new List<Student>
+        {
+            new Student() { Id = 1, Name = "John", Age = 18 } ,
+            new Student() { Id = 2, Name = "Steve",  Age = 21 } ,
+            new Student() { Id = 3, Name = "Bill",  Age = 25 } ,
+            new Student() { Id = 4, Name = "Ram" , Age = 20 } ,
+            new Student() { Id = 5, Name = "Ron" , Age = 31 } ,
+            new Student() { Id = 6, Name = "Chris" , Age = 17 } ,
+            new Student() { Id = 7, Name = "Rob" , Age = 19 }
+        };
+
+        public List<Student> GetAll()
+        {
+            return studentList;
+        }
+
+        public Student GetById(int id)
+        {
+            return studentList.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Delete(int id)
+        {
+            var student = studentList.FirstOrDefault(x => x.Id == id);
+            studentList.Remove(student);
+        }
+
+        public void Update(int id, Student updated)
+        {
+            var student = studentList.FirstOrDefault(x => x.Id == id);
+
+            student.Age = updated.Age;
+            student.Name = updated.Name;
+
+            // todo - save student in db
+        }
+
+        public void Add(Student student)
+        {
+            student.Id = GetMaxId();
+
+            studentList.Add(student);
+
+            // todo - save student in db
+        }
+
+        private int GetMaxId()
+        {
+            return studentList.Count + 1;
+        }
+    }
+}
