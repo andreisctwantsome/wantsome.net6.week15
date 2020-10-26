d203c90b5a6da2a3fd68ea2e78b0eb69d3626e0c
sctrandrei <sctr.andrei@gmail.com>
2020-10-26 20:57:47 +0200

    updates

---
+++ Performance/01.Threading/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace _01.Threading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            Thread t = new Thread(() => 
+            {
+                ProcessFile(@"data\file4.txt");
+                ProcessFile(@"data\file5.txt");
+            });
+
+            t.Start();
+
+            ProcessFile(@"data\file1.txt");
+            ProcessFile(@"data\file2.txt");
+            ProcessFile(@"data\file3.txt");
+
+            stopwatch.Stop();
+
+            Console.WriteLine();
+            Console.WriteLine($"Elapsed: {stopwatch.ElapsedMilliseconds} ms");
+        }
+
+        static void MainV2(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            ProcessFile(@"data\file1.txt");
+            ProcessFile(@"data\file2.txt");
+            ProcessFile(@"data\file3.txt");
+            ProcessFile(@"data\file4.txt");
+            ProcessFile(@"data\file5.txt");
+
+            stopwatch.Stop();
+
+            Console.WriteLine();
+                Console.WriteLine($"Elapsed: {stopwatch.ElapsedMilliseconds} ms");
+        }
+
+        static void ProcessFile(string path)
+        {
+            var lines = File.ReadAllLines(path).ToList();
+            var count = lines.Count;
+
+            lines.RemoveAll(x => x == "3");
+
+            var nrAp1 = lines.Where(x => x == "1").Count();
+            var nrAp2 = lines.Where(x => x == "2").Count();
+            var nrAp3 = lines.Where(x => x == "3").Count();
+
+            Console.WriteLine($"{path}, {count} lines; NrAp of 1 = {nrAp1}; 2 = {nrAp2}; 3 = {nrAp3}");
+        }
+    }
+}
---
+++ Performance/CompareHashsetList/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace CompareHashsetList
+{
+    class Program
+    {
+        const int N = 100000;
+
+        static void Main(string[] args)
+        {
+            List();
+            HashSet();
+        }
+
+        static void List()
+        {
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            var list = new List<string>();
+
+            for (int i = 0; i < N; i++)
+            {
+                var s = i.ToString();
+                list.Add(s);
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                list.Remove(i.ToString());
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"List: elapsed: {stopwatch.ElapsedMilliseconds} ms");
+        }
+
+        static void HashSet()
+        {
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            var list = new HashSet<string>();
+
+            for (int i = 0; i < N; i++)
+            {
+                var s = i.ToString();
+                list.Add(s);
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                list.Remove(i.ToString());
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"HashSet: elapsed: {stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
